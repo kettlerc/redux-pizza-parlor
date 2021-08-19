585e0c33a2dd8f74089a6922204e04ed65167fe7
Eben Stine <ebenstine@gmail.com>
2021-08-19 10:55:19 -0500

    cset up redux and reducer

--- src/index.js
+++ src/index.js
@@ -2,5 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Redux stuff
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+const pizzaSelectReducer = (state = [], action) => {
+    return state;
+};
+
+
+
+
+
+
+const storeInstance = createStore(
+    combineReducers({
+        pizzaSelectReducer,
+    }),
+    applyMiddleware(logger)
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
