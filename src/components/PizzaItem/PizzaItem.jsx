486936cf1ecb1dd2348bc0591969e9a487df1a69
Eben Stine <ebenstine@gmail.com>
2021-08-19 14:42:33 -0500

    toggle function works

--- src/components/PizzaItem/PizzaItem.jsx
+++ src/components/PizzaItem/PizzaItem.jsx
@@ -1,7 +1,43 @@
 import React, { useState } from 'react';
 
 
-function PizzaItem () {
-    return (<h1>PIZZA ITEM TEST!!!! PIZZA RULZ</h1>)
+function PizzaItem ({pizza}) {
+    const [buttonClick, setButtonClick] = useState(true);
+    const toggleButtonClick = () => {
+        setButtonClick (!buttonClick);
+    }
+    const toggleButton = () => {
+        if (buttonClick){
+            return <button onClick={toggleButtonClick}>Add</button>
+        }
+        else {
+            return <button onClick={toggleButtonClick}>Remove</button>
+        }
+
+    }
+    
+
+    return (
+       
+        <div>
+            <img src={pizza.image_path} />
+            <h3>{pizza.name}</h3>
+            <p>{pizza.description}</p>
+            <h4>{pizza.price}</h4>
+            <div>
+            {toggleButton()}
+            </div>
+        </div>
+        
+        
+            
+            
+            
+        
+        
+
+       
+
+    )
 }
 export default PizzaItem;
