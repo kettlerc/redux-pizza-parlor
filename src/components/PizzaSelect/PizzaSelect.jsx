fac7e5915f745dfc304aab2d39064d5fbfcce8d0
Eben Stine <ebenstine@gmail.com>
2021-08-19 11:21:27 -0500

    wrote dispatch

--- src/components/PizzaSelect/PizzaSelect.jsx
+++ src/components/PizzaSelect/PizzaSelect.jsx
@@ -1,7 +1,31 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import axios from 'axios';
+import { useEffect } from 'react'; 
 
 function PizzaSelect(){
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        fetchPizzas();
+    }, [] );
+
+    const fetchPizzas = () => {
+        axios ({
+            method: 'GET',
+            url: '/pizzas'
+        }).then((response) => {
+            dispatch({
+                type: 'SET_PIZZA_MENU',
+                payload: response.data
+            })
+        }).catch((error) => {
+            console.log('GET error', error);
+        })
+    }
+
+
+
     return (<h1>Our pizza test</h1>)
 
 }
